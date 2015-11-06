cb8a84e525737b5807dc99bb4b9410048e1be70f
Michael Miller <michaelmiller@michaels-macbook-pro.local>
2015-11-05 19:50:33 -0500

    got boxes showing up

--- Breakout/BreakoutViewController.swift
+++ Breakout/BreakoutViewController.swift
@@ -10,32 +10,50 @@ import UIKit
 
 class BreakoutViewController: UIViewController {
 
+    // MARK: Constants
+    private struct Constants {
+        static let heightToWidthRatio: CGFloat = 2/3
+        
+    }
+    
     // MARK: Variables
     @IBOutlet weak var gameView: UIView!
     
-    var boxesPerRow: CGFloat {
-        return 5
-    }
-    
-    var numberOfRows: CGFloat {
-        return 3
-    }
+    var boxesPerRow: CGFloat = 5
+    var numberOfRows: CGFloat = 3
+    var verticalSpacing: CGFloat = 30
+    var horizontalSpacing: CGFloat = 30
+    var blocks = [UIView?]()
     
     var boxSize: CGSize {
-        let w = gameView.bounds.width / (boxesPerRow + 3)
-        let h = gameView.bounds.width * 2/3
+        let w = (gameView.bounds.size.width - (horizontalSpacing * (boxesPerRow + 1))) / boxesPerRow
+        let h = w * Constants.heightToWidthRatio
         let size = CGSize(width: w, height: h)
         return size
     }
     
     // MARK: Methods
     func updateUI() {
-        let boxFrame = CGRect(origin: CGPointZero, size: boxSize)
-        let block = UIView(frame: boxFrame)
-        gameView.addSubview(block)
-        block.backgroundColor = UIColor.brownColor()
+        for block in blocks {
+            
+        }
     }
     
+    func setupBoxes() {
+        
+        for var rowCount = 1; rowCount <= Int(numberOfRows); ++rowCount {
+            for var boxCount = 1; boxCount <= Int(boxesPerRow); ++boxCount {
+                let xLocation = horizontalSpacing * CGFloat(boxCount) + boxSize.width * (CGFloat(boxCount) - 1)
+                let yLocation = verticalSpacing * CGFloat(rowCount) + boxSize.height * (CGFloat(rowCount) - 1)
+                let boxOrigin = CGPoint(x: xLocation, y: yLocation)
+                let block = UIView(frame: CGRect(origin: boxOrigin, size: boxSize))
+                block.backgroundColor = UIColor.blueColor()
+                gameView.addSubview(block)
+                blocks.append(block)
+            }
+        }
+        updateUI()
+    }
     
     // MARK: Lifecycle
     override func viewDidLoad() {
@@ -43,7 +61,13 @@ class BreakoutViewController: UIViewController {
         
     }
 
-    override func viewWillLayoutSubviews() {
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(true)
+        setupBoxes()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
         updateUI()
     }
     
