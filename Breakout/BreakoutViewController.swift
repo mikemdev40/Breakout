2dc59a25454279ca29702eabd4e9a2ff87b84c52
Michael Miller <michaelmiller@michaels-macbook-pro.local>
2015-11-06 07:16:51 -0500

    paddle on screen and situated

--- Breakout/BreakoutViewController.swift
+++ Breakout/BreakoutViewController.swift
@@ -13,62 +13,71 @@ class BreakoutViewController: UIViewController {
     // MARK: Constants
     private struct Constants {
         static let heightToWidthRatio: CGFloat = 2/3
-        static let topIndentBeforeFirstRow: CGFloat = 30
-        
+        static let topIndentBeforeFirstRow: CGFloat = 20
+        static let topPortionOfScreenForBlocks: CGFloat = 0.5
+        static let paddleHeight: CGFloat = 15
     }
+    
+    // MARK: Adjustable Variables
+    var blocksPerRow: CGFloat = 6
+    var numberOfRows: CGFloat = 4
+    var verticalSpacing: CGFloat = 10
+    var horizontalSpacing: CGFloat = 10
+    var paddleWidth: CGFloat = 75
     
     // MARK: Variables
     @IBOutlet weak var gameView: UIView!
-    
-    var blocksPerRow: CGFloat = 5
-    var numberOfRows: CGFloat = 3
-    var verticalSpacing: CGFloat = 10
-    var horizontalSpacing: CGFloat = 10
     var blocks = [UIView?]()
+    var paddle = UIView()
     
-    var boxSize: CGSize {
+    private var blockSize: CGSize {
         let w = (gameView.bounds.size.width - (horizontalSpacing * (blocksPerRow + 1))) / blocksPerRow
-        let h = w * Constants.heightToWidthRatio
+        let h = min(w * Constants.heightToWidthRatio, (gameView.bounds.size.height * Constants.topPortionOfScreenForBlocks - (verticalSpacing * (numberOfRows + 1))) / numberOfRows)
         let size = CGSize(width: w, height: h)
         return size
     }
     
     // MARK: Methods
-    func updateUI() {
+    private func updateUI() {
         var index = 0
         for row in 1...Int(numberOfRows) {
             for block in 1...Int(blocksPerRow) {
-                let xLocation = horizontalSpacing * CGFloat(block) + boxSize.width * (CGFloat(block) - 1)
-                let yLocation = verticalSpacing * CGFloat(row) + boxSize.height * (CGFloat(row) - 1) + Constants.topIndentBeforeFirstRow
-                blocks[index]!.frame.size = boxSize  // we can adjust settings on an array of subviews because classes are REFERENCE types so when we change one of the properties of the subview in the blocks array, we are changing the property of the REAL subview (not a copy of it)!!!
-                blocks[index]!.frame.origin = CGPoint(x: xLocation, y: yLocation)
+                let xLocation = horizontalSpacing * CGFloat(block) + blockSize.width * (CGFloat(block) - 1)
+                let yLocation = verticalSpacing * CGFloat(row) + blockSize.height * (CGFloat(row) - 1) + Constants.topIndentBeforeFirstRow
+                blocks[index]!.frame.size = blockSize  // we can adjust settings on an array of subviews because classes are REFERENCE types so when we change one of the properties of the subview in the blocks array, we are changing the property of the REAL subview (not a copy of it)!!!
+                
+                blocks[index]!.frame.origin = CGPoint(x: xLocation, y: yLocation)  //FRAME, not bounds!!!  it took me two hours to figure out why the blocks weren't printing; don't forget that we have to use FRAME to adjust to the location of the view in the superview!!!!  (see lecture 5 notes)
+                
                 blocks[index]!.backgroundColor = UIColor.redColor()
+       
         //CODE BELOW NOT NEEDED since we can use the blocks array to access the subview properties, since classes are REFERENCE TYPES; however, the code below works just fine!!
-                //gameView.subviews[index].frame.size = boxSize
+                //gameView.subviews[index].frame.size = blockSize
                 //gameView.subviews[index].frame.origin = CGPoint(x: xLocation, y: yLocation)
                 //gameView.subviews[index].backgroundColor = UIColor.redColor()
+                
                 index++
             }
         }
-
     }
     
-    func setupBoxes() {
+    private func placePaddle() {
+        paddle.frame.size.height = Constants.paddleHeight
+        paddle.frame.size.width = paddleWidth
+        let xLocation = gameView.frame.midX - paddleWidth / 2
+        let yLocation = gameView.frame.maxY - paddle.frame.height
+        paddle.frame.origin = CGPoint(x: xLocation, y: yLocation)
+        paddle.backgroundColor = UIColor.greenColor()
+    }
+    
+    private func setupBoxes() {
         for var count = 1; count <= Int(numberOfRows * blocksPerRow); ++count {
             let block = UIView()
             gameView.addSubview(block)
             blocks.append(block)
         }
-//        for var rowCount = 1; rowCount <= Int(numberOfRows); ++rowCount {
-//            for var boxCount = 1; boxCount <= Int(boxesPerRow); ++boxCount {
-//                let xLocation = horizontalSpacing * CGFloat(boxCount) + boxSize.width * (CGFloat(boxCount) - 1)
-//                let yLocation = verticalSpacing * CGFloat(rowCount) + boxSize.height * (CGFloat(rowCount) - 1) //+ Constants.topIndentBeforeFirstRow
-//                let boxOrigin = CGPoint(x: xLocation, y: yLocation)
-//                let block = UIView(frame: CGRect(origin: boxOrigin, size: boxSize))
-//                gameView.addSubview(block)
-//                blocks.append(block)
-//            }
-//        }
+        gameView.addSubview(paddle)
+        //blocks.append(paddle)  //NOT added to the blocks array!!!  (duh)
+        print(blocks.count)
     }
     
     // MARK: Lifecycle
@@ -83,8 +92,8 @@ class BreakoutViewController: UIViewController {
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        placePaddle()
         updateUI()
-      //  print("viewDidLayout \(gameView.bounds)")
     }
     
     override func didReceiveMemoryWarning() {
